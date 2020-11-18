c25252d38a8e9e9f49fb23bbed20818f55e962be
Adam Pagels <pagels@hotmail.ca>
2020-11-18 17:22:01 -0500

    Add IBOutlet for recordButton and stopRecordingButton

--- pitchPerfect/ViewController.swift
+++ pitchPerfect/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed")
         recordingLabel.text = "Recording in Progress"
