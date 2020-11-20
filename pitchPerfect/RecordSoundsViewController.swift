7650974106aa2adf353f27f1825427457c3bab0d
Adam Pagels <pagels@hotmail.ca>
2020-11-20 15:45:26 -0500

    Pass recordings from RecordSoundsViewController to PlaySoundsViewController

--- pitchPerfect/RecordSoundsViewController.swift
+++ pitchPerfect/RecordSoundsViewController.swift
@@ -57,4 +57,11 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
 }
